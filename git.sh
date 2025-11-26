6afef61c4a482968a5206e61196554b3c89a549b
Dominic Brekau <dominic.brekau@credativ.de>
2025-11-26 12:16:33 +0100

    recursion protection

--- git.sh
+++ git.sh
@@ -3,8 +3,15 @@
 git.bin "$@"
 EXITCODE=$?
 
+# Recursion filter, if git-crypt is triggering git
+if [ "${GIT_CRYPT_BYPASS:-0}" = "1" ];
+  exit $EXITCODE
+fi
+
 if ([ "$1" = "fetch" ] || [ "$1" = "checkout" ]) && [ -d .git-crypt ]; then
-GNUPGHOME=/app/config/gpg/keys git-crypt unlock
+  export GIT_CRYPT_BYPASS=1
+  export GNUPGHOME=/app/config/gpg/keys
+  git-crypt unlock
 fi
 
 exit $EXITCODE
